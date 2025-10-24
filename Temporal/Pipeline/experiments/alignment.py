9b7189e1ace2eebb2eaca4927ce922f47d0499cb
j-balkovec <jakob.balkovec@gmail.com>
2025-10-24 14:33:30 -0700

    impl: ensuring alignment

---
+++ Temporal/Pipeline/experiments/alignment.py
@@ -0,0 +1,12 @@
+# Jakob Balkovec
+# The purpose of this script is to make sure the alignment is correct
+
+# Alignment in question:
+#      * Raw coordinates from the dataset
+#      * Satellite
+
+# Spokane
+
+# Darrington
+
+# Quinault
